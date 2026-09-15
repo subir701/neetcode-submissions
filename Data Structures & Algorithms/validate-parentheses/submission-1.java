e40322ab89876da91749d587459019c78379a62b
Subir Adhikari <107720819+subir701@users.noreply.github.com>
2026-09-15 13:16:59 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c: s.toCharArray()) {
+            if(c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else {
+                if(stack.isEmpty()) return false;
+                char top = stack.peek();
+                if((c == ']' && top == '[') || (c == ')' && top == '(') || (c == '}' && top == '{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+                
+            }
+        }
+        return stack.isEmpty() ? true : false;
+    }
+}
